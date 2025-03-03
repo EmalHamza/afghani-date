91d0d8b8a1c0a8f014506a5b89ba7ee275977240
EmalHamza1 <83998973+EmalHamza1@users.noreply.github.com>
2025-03-03 14:32:30 +0430

    Updated Afghani Date converter for Laravel

--- src/AfghaniDate.php
+++ src/AfghaniDate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EmalHamza\AfghaniDate;
+namespace App\Helpers;
 
 use Carbon\Carbon;
 
@@ -9,34 +9,31 @@ class AfghaniDate
     /**
      * Convert Gregorian date to Afghan (Solar Hijri) Date.
      *
-     * @param string $gregorianDate
+     * @param string $gregorianDate (YYYY-MM-DD format)
      * @return string
      */
     public static function toAfghaniDate($gregorianDate)
     {
-        $gregorian = Carbon::parse($gregorianDate);
+        $date = Carbon::parse($gregorianDate);
+        $gregorianYear = $date->year;
+        $gregorianMonth = $date->month;
+        $gregorianDay = $date->day;
 
-        // Define the Afghan Solar Hijri conversion logic
-        $gregorianYear = $gregorian->year;
-        $gregorianMonth = $gregorian->month;
-        $gregorianDay = $gregorian->day;
+        // Define the start of the Afghan year (Nowruz on March 19)
+        $startOfYear = Carbon::create($gregorianYear, 2, 19); 
+        $afghaniYear = $gregorianYear - 621;
 
-        // Determine the starting date of the Afghan year (Nowruz is March 21)
-        $startOfYear = Carbon::createFromDate($gregorianYear, 3, 21);
-
-        // If the given date is before Nowruz, move to the previous Afghan year
-        if ($gregorian->lt($startOfYear)) {
-            $afghaniYear = $gregorianYear - 622;
-            $startOfYear = Carbon::createFromDate($gregorianYear - 1, 3, 21);
-        } else {
-            $afghaniYear = $gregorianYear - 621;
+        // If the date is before Nowruz, move to the previous Afghan year
+        if ($date->lessThan($startOfYear)) {
+            $afghaniYear--;
+            $startOfYear = Carbon::create($gregorianYear - 1, 2, 19);
         }
 
-        // Calculate the days difference since Nowruz
-        $daysDifference = $startOfYear->diffInDays($gregorian);
+        // Calculate the number of days since Nowruz
+        $daysDifference = $startOfYear->diffInDays($date);
 
         // Convert to Afghan month and day
-        list($afghaniMonth, $afghaniDay) = self::getAfghaniMonthAndDay($daysDifference);
+        ['afghaniMonth' => $afghaniMonth, 'afghaniDay' => $afghaniDay] = self::getAfghaniMonthAndDay($daysDifference);
 
         return "{$afghaniYear}/{$afghaniMonth}/{$afghaniDay}";
     }
@@ -45,7 +42,7 @@ class AfghaniDate
      * Get Afghan month and day from the difference in days.
      *
      * @param int $daysDifference
-     * @return array [month, day]
+     * @return array
      */
     private static function getAfghaniMonthAndDay($daysDifference)
     {
@@ -54,12 +51,12 @@ class AfghaniDate
 
         foreach ($monthLengths as $length) {
             if ($daysDifference < $length) {
-                return [$month, $daysDifference + 1]; // Days are 1-based
+                return ['afghaniMonth' => $month, 'afghaniDay' => $daysDifference + 1];
             }
             $daysDifference -= $length;
             $month++;
         }
 
-        return [$month, $daysDifference + 1];
+        return ['afghaniMonth' => $month, 'afghaniDay' => $daysDifference + 1];
     }
 }
