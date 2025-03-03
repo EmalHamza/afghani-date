ce12f9a36bafde2fc147085176608102df71a941
EmalHamza1 <83998973+EmalHamza1@users.noreply.github.com>
2025-03-03 15:09:32 +0430

    Updated Afghani Date converter for Laravel

--- src/AfghaniDate.php
+++ src/AfghaniDate.php
@@ -19,14 +19,14 @@ class AfghaniDate
         $gregorianMonth = $date->month;
         $gregorianDay = $date->day;
 
-        // Define the start of the Afghan year (Nowruz on March 19)
-        $startOfYear = Carbon::create($gregorianYear, 3, 19); // this is
+        // Define the start of the Afghan year (Nowruz on March 21)
+        $startOfYear = Carbon::create($gregorianYear, 3, 21); // this is
         $afghaniYear = $gregorianYear - 621;
 
         // If the date is before Nowruz, move to the previous Afghan year
         if ($date->lessThan($startOfYear)) {
             $afghaniYear--;
-            $startOfYear = Carbon::create($gregorianYear - 1, 3, 19);
+            $startOfYear = Carbon::create($gregorianYear - 1, 3, 21);
         }
 
         // Calculate the number of days since Nowruz
